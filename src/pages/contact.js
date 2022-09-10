292b8dab7bfca156b44a2a20dc8d027039c62708
Sina Atalay <sinatalay@hotmail.com>
2022-09-10 18:50:36 +0300

    feat: add Google reCAPTCHA to contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,8 @@
 import React from "react"
 import Layout from "../components/Layout"
+import ReCAPTCHA from "react-google-recaptcha"
+
+const recaptchaRef = React.createRef()
 
 export default function Contact() {
   return (
@@ -11,6 +14,9 @@ export default function Contact() {
       <main>
         <form
           className="mt-8"
+          onSubmit={() => {
+            recaptchaRef.current.execute()
+          }}
           action="https://getform.io/f/156b4f25-b9bb-4e5c-88f1-20c4b106a9d2"
           method="POST"
         >
@@ -24,6 +30,7 @@ export default function Contact() {
                 type="name"
                 autoComplete="name"
                 placeholder="John Doe"
+                required
               />
             </label>
           </div>
@@ -38,6 +45,7 @@ export default function Contact() {
                 type="email"
                 autoComplete="email"
                 placeholder="john@doe.com"
+                required
               />
             </label>
           </div>
@@ -48,10 +56,16 @@ export default function Contact() {
                 className="no-resize appearance-none block w-full bg-neutral-200 text-neutral-700 border rounded py-3 px-4 mb-3 leading-tight focus:bg-neutral-50 h-48 resize-none"
                 name="message"
                 id="message"
+                required
               ></textarea>
             </label>
           </div>
           <div className="md:flex md:items-center md:w-1/3">
+            <ReCAPTCHA
+              ref={recaptchaRef}
+              size="invisible"
+              sitekey="6LfD6uohAAAAAJOUdUH99Eqd-WtbUMZOOeSRWL6k"
+            />
             <button
               className="font-normal shadow bg-primary-500 hover:bg-primary-900 transition duration-200 ease-out focus:shadow-outline focus:outline-none text-secondary-500 hover:text-secondary-400 py-2 px-4 rounded"
               type="submit"
